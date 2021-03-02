594f5c8c469299d5c05184d9aa04eae1dd373cfe
Marcus <marcus.jeremy.chu@gmail.com>
2021-03-01 19:42:51 -0800

    implemented threading, tested with two sensors

--- ultrasonic_sense.c
+++ ultrasonic_sense.c
@@ -2,6 +2,28 @@
 #include <bcm2835.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+// completes one pulse cycle to get the range, needs a trigger first
+// this is for a single pin
+long get_range(int echo_pin) {
+	long start_time, end_time, range_cm;
+    while (bcm2835_gpio_lev(echo_pin) == LOW) {
+    	start_time = get_time_microsec();
+    }
+
+    while (bcm2835_gpio_lev(echo_pin) == HIGH) {
+    	end_time = get_time_microsec();
+    }
+
+    range_cm = (end_time - start_time) / 58;
+    return range_cm;
+}
+
+void *threaded_range(void *echo_pin) {
+	return (void*) get_range((int) echo_pin);
+}
 
 int main(void) {
 
@@ -9,35 +31,26 @@ int main(void) {
   if (!bcm2835_init())
     return 1;
 
-    // set pins to respective functions
-  printf("Attempting to fsel pins...\n");
   bcm2835_gpio_fsel(PIN_TRIGGER, BCM2835_GPIO_FSEL_OUTP);
   bcm2835_gpio_write(PIN_TRIGGER, LOW);
   bcm2835_gpio_fsel(PIN_ECHO_1, BCM2835_GPIO_FSEL_INPT);
   bcm2835_gpio_fsel(PIN_ECHO_2, BCM2835_GPIO_FSEL_INPT);
-
-  long start_time, end_time, range_cm;
-  int max = 80, check;
-  printf("Initialized, starting while loop\n");
   delay(500);
 
+
   while (1) {
-    pulse_trigger();
+	pthread_t thread_id_1, thread_id_2;
+	void *thread_ret_1, *thread_ret_2;
+	pulse_trigger();
 
-    // I might need to make a separate thread for each echo pin to track
-    // length of the echo pulse
-    while (bcm2835_gpio_lev(PIN_ECHO_1) == LOW) {
-    	start_time = get_time_microsec();
-    }
+	pthread_create(&thread_id_1, NULL, threaded_range, (void*)PIN_ECHO_1);
+	pthread_create(&thread_id_2, NULL, threaded_range, (void*)PIN_ECHO_2);
 
-    while (bcm2835_gpio_lev(PIN_ECHO_1) == HIGH) {
-    	end_time = get_time_microsec();
-    }
-    //("Start_time: %ld, ", start_time);
-    //printf("End_time: %ld\n", end_time);
+	pthread_join(thread_id_1, &thread_ret_1);
+	pthread_join(thread_id_2, &thread_ret_2);
 
-    range_cm = (end_time - start_time) / 58;
-    printf("Range: %ld\n", range_cm);
+    printf("Range 1: %ld\n", (long)thread_ret_1);
+    printf("Range 2: %ld\n", (long)thread_ret_2);
   }
 
   return 0;
